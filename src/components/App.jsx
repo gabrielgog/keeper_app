938bac18392170654a807cd7ab7c549f28b4bde2
gabrielgog <gabrielgog96@gmail.com>
2020-11-24 23:09:09 +0100

    add new array method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import Header from './Header'
+import Note from './Note'
+import Footer from './Footer'
+import notes from '../notes';
 
-function App(){
-    return <div><h1>HEllo APp</h1></div>
+
+function createNote(noteItem) {
+    return <Note
+        title={noteItem.title} content={noteItem.content} />
+}
+
+function App() {
+    return (<div>
+        <Header />
+
+        {notes.map(createNote)
+        }
+
+        <Footer />
+
+    </div>
+
+
+
+
+    )
 }
 
 export default App;
---
+++ src/notes.js
@@ -0,0 +1,32 @@
+const note = [
+
+    { key: 1,
+
+
+        title: "Today I will be coding all night untill I am a badass",
+
+        content: "None"
+    }, 
+
+
+    {
+        key:2, 
+
+        title: "We will fry rice today", 
+
+
+        content: "We should please fry"
+    },
+
+
+    {
+        key:3, 
+
+        title: "We will fry rice today", 
+
+
+        content: "We should please fry"
+    }
+]
+
+export default notes;
